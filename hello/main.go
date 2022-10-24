9ce3ba86e20324f713eafe5e1a584b7a3f7239b8
Manjunath Reddy <manju16832003@gmail.com>
2022-10-24 20:50:54 +0200

    Enhance demo app

--- hello/main.go
+++ hello/main.go
@@ -13,5 +13,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Running demo app. Press Ctrl+C to exit...")
+	fmt.Println("Server is running at http://localhost:8888")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
